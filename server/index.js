8020568cdc09da3eebfe07556e25025df9c325a4
ezrafrost7 <71141893+ezrafrost7@users.noreply.github.com>
2022-02-25 22:30:25 -0700

    started building some backend components

--- server/index.js
+++ server/index.js
@@ -1,10 +1,20 @@
 // server/index.js
-
 const express = require("express");
-
 const PORT = process.env.PORT || 3001;
-
 const app = express();
+
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./data.db', sqlite3.OPEN_READWRITE, (err) => {
+    if (err) return console.error(err.message);
+
+    console.log('connection successful')
+});
+
+db.run('CREATE TABLE users(first_name, last_name, email)')
+
+db.close((err) => {
+    if (err) return console.error(err.message);
+});
 
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
